19ca268ea5026c1725857d164b175870135c332b
Kalevitutar <tanyamthomas@yahoo.com>
2024-04-05 13:12:10 -0400

    adding clearer insertion sort code

--- exercises.js
+++ exercises.js
@@ -99,3 +99,20 @@ console.log("Unsorted Words:", unsortedNewWords);
 insertionSortWords(unsortedNewWords);
 
 console.log("Sorted Words:", unsortedNewWords);
+
+
+// ANOTHER INSERTION SORT CODE
+
+function clearerInsertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let temp = arr[i];
+        let j = i - 1;
+
+        while (j >= 0 && arr[j] > temp) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp
+    }
+    return arr;
+}
